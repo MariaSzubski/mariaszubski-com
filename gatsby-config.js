ab4042f93f5f27a9994562b263b4d4d20ccda24c
Maria Szubski <hello@mariaszubski.com>
2019-10-13 01:37:13 -0400

    Set up config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Maria Szubski - Freelance Front End Developer - Cincinnati, OH`,
+    description: `Cincinnati, OH - Web Development Contractor`,
+    author: `Maria Szubski Digital, LLC`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    'gatsby-plugin-eslint',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,22 +17,43 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Maria Szubski - Freelance Front End Developer - Cincinnati, OH - Web Development Contractor`,
+        short_name: `Maria Szubski Digital, LLC`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#121D28`,
+        theme_color: `#121D28`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/content/`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-webfonts`,
+    //   options: {
+    //     fonts: {
+    //       google: [
+    //         {
+    //           family: 'Nunito',
+    //           variants: ['300', '400', '600', '700'],
+    //           //fontDisplay: 'swap',
+    //           //strategy: 'selfHosted' // 'base64' || 'cdn'
+    //         },
+    //       ],
+    //     },
+    //     useMinify: true,
+    //     usePreload: true,
+    //     usePreconnect: true,
+    //     //formats: ['woff2', 'woff'],
+    //   },
+    // },
   ],
 }
