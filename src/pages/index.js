a124bc99393f5a9abc2c5d8b5bf32c49a1e92ecc
Maria Szubski <hello@mariaszubski.com>
2020-07-28 10:12:22 -0400

    Set up Contentful landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,19 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const IndexPage = ( data ) => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>{data.data.contentfulPage.title}</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Welcome</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
-
-export const query = graphql`
-  query {
-    contentfulPage(contentful_id: {eq: "7KKQ9SlaHWAvVjXwpmF8QC"}) {
-      title
-      slug
-      modules {
-      ... on ContentfulSkillGroup {
-        id
-        title
-        displayLabel
-        darkMode
-        skill
-      }
-    }
-    }
-  }
-`
