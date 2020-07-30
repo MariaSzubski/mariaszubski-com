ea589229ce5a18b025e235baba2bbea427446c98
Maria Szubski <hello@mariaszubski.com>
2020-07-30 11:06:55 -0400

    Fix merge conflicts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import SEO from '../components/layout/seo'
 
 const IndexPage = () => {
   return (
